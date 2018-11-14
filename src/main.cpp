0c33400e732388c6e4491b701ff3ba13ef416425
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2018-11-14 23:21:38 +0100

    010: added main View

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,31 @@ using namespace game;
 
 int main()
 {
-    // Create window
-    sf::VideoMode vm(1920, 1080);
-    sf::RenderWindow window(vm, "Mad Dead", sf::Style::Fullscreen);
+    State state{State::GAME_OVER};
+
+    sf::Vector2f resolution;
+    resolution.x = sf::VideoMode::getDesktopMode().width;
+    resolution.y = sf::VideoMode::getDesktopMode().height;
+
+    sf::RenderWindow window(sf::VideoMode(resolution.x, resolution.y), "Mad Dead",
+                            sf::Style::Fullscreen);
+    sf::View viewMain(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+    sf::Clock clock;
+    sf::Time gameTimeTotal;
+
+    sf::Vector2f mouseWorldPosition;
+    sf::Vector2i mouseScreenPosition;
+
     Player player;
+
+    sf::IntRect arena;
+
+    viewMain.setCenter(player.getCenter());
+    window.setView(viewMain);
     while (window.isOpen())  // Game loop
     {
+        viewMain.rotate(180);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
         window.clear();
         window.draw(player.getSprite());
