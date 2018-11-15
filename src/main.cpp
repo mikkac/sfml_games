577f06f7a8c17c59ecf845403d0af5b702b7beb7
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2018-11-15 23:04:43 +0100

    011: able to spin the player around

--- src/main.cpp
+++ src/main.cpp
@@ -24,14 +24,99 @@ int main()
 
     sf::IntRect arena;
 
-    viewMain.setCenter(player.getCenter());
-    window.setView(viewMain);
+    // viewMain.setCenter(player.getCenter());
     while (window.isOpen())  // Game loop
     {
-        viewMain.rotate(180);
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.code == sf::Keyboard::Return && state == State::PLAY)
+                    state = State::PAUSE;
+                else if (event.key.code == sf::Keyboard::Return && state == State::GAME_OVER)
+                    state = State::LEVEL_UP;
+                else if (event.key.code == sf::Keyboard::Return && state == State::PAUSE)
+                {
+                    state = State::PLAY;
+                    clock.restart();
+                }
+
+                if (state == State::PLAY)
+                {
+                }
+            }
+        }  // end event polling
+
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
-        window.clear();
-        window.draw(player.getSprite());
+
+        // Handle WSAD
+        if (state == State::PLAY)
+        {
+            player.moveUp(sf::Keyboard::isKeyPressed(sf::Keyboard::W));
+            player.moveDown(sf::Keyboard::isKeyPressed(sf::Keyboard::S));
+            player.moveLeft(sf::Keyboard::isKeyPressed(sf::Keyboard::A));
+            player.moveRight(sf::Keyboard::isKeyPressed(sf::Keyboard::D));
+        }
+
+        // Handle LEVEL_UP state
+        if (state == State::LEVEL_UP)
+        {
+            if (event.key.code == sf::Keyboard::Num1) state = State::PLAY;
+            if (event.key.code == sf::Keyboard::Num2) state = State::PLAY;
+            if (event.key.code == sf::Keyboard::Num3) state = State::PLAY;
+            if (event.key.code == sf::Keyboard::Num4) state = State::PLAY;
+            if (event.key.code == sf::Keyboard::Num5) state = State::PLAY;
+            if (event.key.code == sf::Keyboard::Num6) state = State::PLAY;
+
+            if (state == State::PLAY)
+            {
+                // Preapre the level
+                arena.width = 500;
+                arena.height = 500;
+                arena.left = 0;
+                arena.top = 0;
+
+                int tileSize{50};
+
+                player.spawn(resolution, arena, tileSize);
+                clock.restart();
+            }
+        }  // end LEVEL_UP
+
+        // Update the frame
+        if (state == State::PLAY)
+        {
+            sf::Time dt = clock.restart();
+            gameTimeTotal += dt;
+            float dtAsSeconds = dt.asSeconds();
+            mouseScreenPosition = sf::Mouse::getPosition();
+
+            // Convert mouse postition to world coords
+            mouseWorldPosition = window.mapPixelToCoords(mouseScreenPosition, viewMain);
+
+            player.update(dtAsSeconds, mouseScreenPosition);
+            sf::Vector2f playerPosition(player.getCenter());
+            viewMain.setCenter(player.getCenter());
+        }  // end updating the frame
+
+        // Draw the scene
+        if (state == State::PLAY)
+        {
+            window.clear();
+            window.setView(viewMain);
+            window.draw(player.getSprite());
+        }
+
+        if (state == State::LEVEL_UP)
+        {
+        }
+        if (state == State::PAUSE)
+        {
+        }
+        if (state == State::GAME_OVER)
+        {
+        }
         window.display();
     }
     return 0;
