67b2c784ae8468a993042a6361f2e4a615acedba
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2018-12-15 18:28:29 +0100

    004: removed build directory once again

--- build/CMakeFiles/bob_was_alone-format.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bob_was_alone-format"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bob_was_alone-format.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/bob_was_alone-tidy.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bob_was_alone-tidy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bob_was_alone-tidy.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
