ec6203a268585f28e2847e3aed27575d624768dc
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2018-12-17 00:16:53 +0100

    018: added Level class

--- inc/level_manager.h
+++ inc/level_manager.h
@@ -1,31 +1,44 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <string>
 #include <vector>
 
 namespace game
 {
 using namespace sf;
-using LevelMatrix = std::vector<std::vector<int>>;
+
+struct Level {
+    std::string path_to_file{};
+    Vector2u size{};
+    Vector2f start_pos{};
+    float base_time_limit{};
+
+    int** load_from_file();
+
+  private:
+    Vector2u find_out_size(std::ifstream& input_file);
+    int** create_array(std::ifstream& input_file);
+};
+
+Level load_level(unsigned index);
 
 class LevelManager
 {
   public:
-    LevelMatrix next_level(VertexArray& vert_arr);
+    int** next_level(VertexArray& vert_arr);
 
-    Vector2u get_level_size() const { return level_size_; }
-    Vector2f get_start_pos() const { return start_pos; }
-    float get_time_limit() const { return base_time_limit_; }
+    Vector2u get_level_size() const { return level_.size; }
+    Vector2f get_start_pos() const { return level_.start_pos; }
+    float get_time_limit() const { return level_.base_time_limit; }
     int get_current_level() const { return current_level_; }
 
   private:
-    const int kTileSize{50};
-    const int kVertsInQuad{4};
-    const int kNumLevels{4};
+    const unsigned kTileSize{50};
+    const unsigned kVertsInQuad{4};
+    const unsigned kNumLevels{4};
 
-    Vector2u level_size_;
-    Vector2f start_pos_;
+    Level level_;
     float time_modifier_{1.f};
-    float base_time_limit_{0.f};
     unsigned current_level_{0};
 };
 } // namespace game
--- src/level_manager.cpp
+++ src/level_manager.cpp
@@ -1,8 +1,93 @@
 #include "level_manager.h"
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
 
-namespace game 
+namespace game
 {
-LevelMatrix LevelManager::next_level(VertexArray& vert_arr) {
+Level load_level(unsigned index) {
+    Level result{};
+    std::string path_to_file{};
+    switch (index) {
+        case 1:
+            result.path_to_file = "res/levels/levels1.txt";
+            result.start_pos = Vector2f(100.f, 100.f);
+            result.base_time_limit = 30.f;
+            break;
+        case 2:
+            result.path_to_file = "res/levels/levels2.txt";
+            result.start_pos = Vector2f(100.f, 3600.f);
+            result.base_time_limit = 100.f;
+            break;
 
+        case 3:
+            result.path_to_file = "res/levels/levels3.txt";
+            result.start_pos = Vector2f(1250.f, 0.f);
+            result.base_time_limit = 30.f;
+            break;
+
+        case 4:
+            result.path_to_file = "res/levels/levels4.txt";
+            result.start_pos = Vector2f(50.f, 200.f);
+            result.base_time_limit = 50.f;
+            break;
+    }
+    return result;
+}
+
+int** Level::load_from_file() {
+    std::ifstream input_file{path_to_file};
+
+    size = find_out_size(input_file);
+    int** array_level = create_array(input_file);
+    
+    input_file.close();
+
+    return array_level;
+}
+
+Vector2u Level::find_out_size(std::ifstream& input_file) {
+    Vector2u result{};
+    std::string content;
+
+    input_file.clear();
+    input_file.seekg(0, std::ios::beg);
+
+    while (getline(input_file, content)) ++(result.y);
+    result.x = content.length();
+    return result;
+}
+
+int** Level::create_array(std::ifstream& input_file) {
+    input_file.clear();
+    input_file.seekg(0, std::ios::beg);
+    int** array_level = nullptr;
+    try {
+        array_level = new int*[size.y];
+        for (unsigned idx = 0; idx < size.y; ++idx) array_level[idx] = new int[size.x];
+    } catch (std::exception& error) {
+        std::cerr << error.what();
+        return nullptr;
+    }
+
+    std::string row{};
+    int y{};
+
+    while(input_file >> row) {
+        for(unsigned x = 0; x < row.length(); ++x) {
+            const char value = row[x];
+            array_level[y][x] = atoi(&value);
+        }
+        ++y;
+    }
+    return array_level;
+}
+
+int** LevelManager::next_level(VertexArray& vert_arr) {
+    if (++current_level_ > kNumLevels) {
+        current_level_ = 1;
+        time_modifier_ -= 0.1f;
+    }
+    //TODO finish implementation
 }
 } // namespace game
