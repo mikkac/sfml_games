8691b527ed0754a8f351150d02fae751662810f4
Mikolaj Kaczmarek <m.kaczmarek9@gmail.com>
2018-12-20 22:53:42 +0100

    028: CollisionDetection- implemented all methods

--- inc/collision_detection.h
+++ inc/collision_detection.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "playable_character.h"
-
+#include "level_manager.h"
+#include "engine.h" //TODO move all global constats to utils.h
 namespace game
 {
+enum class BlockType { INVISIBLE, REGULAR, FIRE, WATER, ENDGAME };
+
 class CollisionDetection
 {
   public:
-    bool detect(const LevelManager& lvl_man, PlayableCharacter& character);
+    bool detect(LevelManager* level_manager, PlayableCharacter* character);
 
   private:
     void create_collision_zone();
     void handle_leaving_level();
+    void set_current_block(const Vector2i coords);
     void regular_block();
     void fire_and_water_block();
     bool endgame_block();
@@ -18,6 +22,8 @@ class CollisionDetection
   private:
     LevelManager* level_manager_{nullptr};
     PlayableCharacter* character_{nullptr};
+    FloatRect current_block_{0.f, 0.f, kTileSize, kTileSize};
+    BlockType current_block_type_{};
     Vector2i start_zone_{};
     Vector2i end_zone_{};
 };
---
+++ src/collision_detection.cpp
@@ -0,0 +1,92 @@
+#include "collision_detection.h"
+#include <iostream>
+namespace game
+{
+bool CollisionDetection::detect(LevelManager* level_manager, PlayableCharacter* character) {
+    level_manager_ = level_manager;
+    character_ = character;
+    bool reached_goal{false};
+    if (level_manager_ && character_) {
+        create_collision_zone();
+        handle_leaving_level();
+
+        // Iterate through all local blocks
+        for (int x = start_zone_.x; x < end_zone_.x; ++x) {
+            for (int y = start_zone_.y; y < end_zone_.y; ++y) {
+                set_current_block(Vector2i{x, y});
+                fire_and_water_block();
+                regular_block();
+                reached_goal = endgame_block();
+            }
+        }
+    } else {
+        std::cerr << "CollisionDetection: LevelManager or Character is nullptr" << std::endl;
+    }
+    return reached_goal;
+}
+
+void CollisionDetection::create_collision_zone() {
+    FloatRect zone{character_->get_position()};
+
+    start_zone_ = Vector2i{static_cast<int>(zone.left / kTileSize) - 1,
+                           static_cast<int>(zone.top / kTileSize) - 1};
+    end_zone_ = Vector2i{static_cast<int>(zone.left / kTileSize) + 2,
+                         static_cast<int>(zone.top / kTileSize) + 3};
+}
+
+void CollisionDetection::handle_leaving_level() {
+    // Make sure we don't test positions outside the screen
+    if (start_zone_.x < 0) start_zone_.x = 0;
+    if (start_zone_.y < 0) start_zone_.y = 0;
+    if (end_zone_.x >= static_cast<int>(level_manager_->get_level_size().x))
+        end_zone_.x = level_manager_->get_level_size().x;
+    if (end_zone_.y >= static_cast<int>(level_manager_->get_level_size().y))
+        end_zone_.y = level_manager_->get_level_size().y;
+
+    // Has this character fallen out of the map?
+    FloatRect level(0.f, 0.f, level_manager_->get_level_size().x * kTileSize,
+                    level_manager_->get_level_size().y * kTileSize);
+    if (not character_->get_position().intersects(level))
+        character_->spawn(level_manager_->get_start_pos(), kGravity);
+}
+
+void CollisionDetection::set_current_block(Vector2i coords) {
+    current_block_.left = coords.x * kTileSize;
+    current_block_.top = coords.y * kTileSize;
+    current_block_type_ = static_cast<BlockType>(level_manager_->get_array_level()[coords.y][coords.x]);
+}
+
+void CollisionDetection::regular_block() {
+    // Check if it's a regular block
+    if (current_block_type_ == BlockType::REGULAR) {
+        if (character_->get_right().intersects(current_block_))
+            character_->stop_right(current_block_.left);
+        else if (character_->get_left().intersects(current_block_))
+            character_->stop_left(current_block_.left);
+
+        if (character_->get_feet().intersects(current_block_))
+            character_->stop_falling(current_block_.top);
+        else if (character_->get_head().intersects(current_block_))
+            character_->stop_jump();
+    }
+}
+
+void CollisionDetection::fire_and_water_block() {
+    // Check if block is fire or water
+    if (current_block_type_ == BlockType::FIRE || current_block_type_ == BlockType::WATER) {
+        if (character_->get_head().intersects(current_block_)) {
+            character_->spawn(level_manager_->get_start_pos(), kGravity);
+            if (current_block_type_ == BlockType::FIRE) {
+                /* play sound */
+            } else {
+                /* play sound */
+            }
+        }
+    }
+}
+
+bool CollisionDetection::endgame_block() {
+    // Check if it's end-game block
+    if (current_block_type_ == BlockType::ENDGAME) return true;
+}
+} // namespace game
